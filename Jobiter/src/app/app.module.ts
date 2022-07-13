6dc5860f19aeecc2ed2c36a0cb6e46f0b6ee36e3
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-13 13:56:58 +0200

    Add Navbar Component

--- Jobiter/src/app/app.module.ts
+++ Jobiter/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './components/signUp/sign-up/sign-up.component';
+import { NavbarComponent } from './components/core/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
