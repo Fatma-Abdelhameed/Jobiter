8c617fec3cc8bef5ca7a42c7b4cf03f206ae18f4
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-13 17:21:24 +0200

    Install and Add Bootstrap

--- Jobiter/src/app/app.module.ts
+++ Jobiter/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './components/signUp/sign-up/sign-up.component';
+import { SignUpComponent } from './components/features/sign-up/sign-up.component';
 import { NavbarComponent } from './components/core/navbar/navbar.component';
 
 @NgModule({
