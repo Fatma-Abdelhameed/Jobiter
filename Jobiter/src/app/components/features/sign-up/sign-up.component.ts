a2fd3f8a00634f2cc4fb324eb3ebf6d7f3ba8cdd
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-14 21:16:30 +0200

    Sign Up Validation

--- Jobiter/src/app/components/features/sign-up/sign-up.component.ts
+++ Jobiter/src/app/components/features/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/_models/company';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  formData = {
+    name:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+    phone:'',
+    address:'',
+    city:'',
+    industry:'',
+    startedAt:'',
+    size:'',
+    imageUrl:'',
+    websiteLink:'',
+    about:''
+  }
+  formErrors = {
+    name:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+    phone:'',
+    address:'',
+    city:'',
+    industry:'',
+    startedAt:'',
+    size:'',
+    imageUrl:'',
+    websiteLink:'',
+    about:''
+  }
+  company:Company = new Company(0, '', '', '', 0, '', '', '', '', 0, '', '', '')
+  show(t:any){
+    console.log(t.invalid)
+  }
+  constructor() { 
+  }
   
   ngOnInit(): void {
   }
