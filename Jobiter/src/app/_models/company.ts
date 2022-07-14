3ec7bcf61fda8ba17a8b4389081f4cbb0ddbef23
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-14 17:53:31 +0200

    Add Company Model

---
+++ Jobiter/src/app/_models/company.ts
@@ -0,0 +1,9 @@
+export class Company {
+    constructor(public id:number, public companyName:string, public email:string, public password:string,
+        public phone:number, public address:string, public city:string, public industry:string, 
+        public startedAt:string, public size:number, public imageUrl:string, public websiteLink:string,
+        public about:string
+    ){
+
+    }
+}
