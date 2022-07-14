e277cbf1d1cfd4c756c6e7ab7dbece6e4c43db73
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-14 21:21:47 +0200

    Add home, all jobs, post new job components

--- Jobiter/src/app/components/features/company/company.module.ts
+++ Jobiter/src/app/components/features/company/company.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AllJobsComponent } from './all-jobs/all-jobs.component';
+import { PostNewJobComponent } from './post-new-job/post-new-job.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    AllJobsComponent,
+    PostNewJobComponent
+  ],
   imports: [
     CommonModule
   ]
