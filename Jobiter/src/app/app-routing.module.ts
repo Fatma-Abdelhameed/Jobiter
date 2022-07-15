5341c662e03cc7f25b8ec208bf18d035bd98e4d7
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-15 13:37:28 +0200

    Add Authentication Service

--- Jobiter/src/app/app-routing.module.ts
+++ Jobiter/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorizedGuard } from './authorized.guard';
 import { LoginComponent } from './components/features/login/login.component';
 import { SignUpComponent } from './components/features/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'company-sign-up',component:SignUpComponent},
-  {path:'company',loadChildren: ()=>import('./components/features/company/company.module').then(m=>m.CompanyModule)}
+  {
+    path:'company',
+    loadChildren: ()=>import('./components/features/company/company.module').then(m=>m.CompanyModule),
+    canActivate:[AuthorizedGuard]
+  }
 ];
 
 @NgModule({
--- Jobiter/src/app/authorized.guard.ts
+++ Jobiter/src/app/authorized.guard.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizedGuard implements CanActivate {
+  constructor(public router : Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      this.router.navigateByUrl("")
+      return false;
   }
   
 }
