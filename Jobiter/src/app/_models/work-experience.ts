79ed3e8a2d1e3195834d6b46ace65861ac5a1be1
unknown <fatmaabdelhameed1999@gmail.com>
2022-07-22 16:01:06 +0200

    Applicant Profile

---
+++ Jobiter/src/app/_models/work-experience.ts
@@ -0,0 +1,5 @@
+export class WorkExperience {
+    constructor(public id:number, public jobTitle:string, public companyName:string, public startDate:string, public endDate:string){
+
+    }
+}
---
+++ Jobiter/src/app/components/features/applicant/applicant-profile/applicant-profile.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Developer } from 'src/app/_models/developer';
+import { WorkExperience } from 'src/app/_models/work-experience';
+import { ApplicantService } from 'src/app/_services/applicant.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
+
+@Component({
+  selector: 'app-applicant-profile',
+  templateUrl: './applicant-profile.component.html',
+  styleUrls: ['./applicant-profile.component.css']
+})
+export class ApplicantProfileComponent implements OnInit {
+  allowNotifications:boolean = false
+  applicantDetails:Developer = new Developer(0, '', '', '', '', '', '', '', '', '', '', '', '')
+  applicantWorkExperience:WorkExperience[] = []
+  constructor(public token:TokenStorageService, public applicantSer:ApplicantService) { }
+  changeNotificationState(state:boolean){
+    this.applicantSer.changeNotificationsState(this.token.getUser().id, state).subscribe({
+      next:()=>{
+        
+      }
+    })
+  }
+  delete(id:number){
+    this.applicantSer.deleteWorkExperience(id).subscribe({
+      next:()=>{
+
+      },
+      error:()=>{
+
+      }
+    })
+  }
+  ngOnInit(): void {
+    this.applicantSer.getNotificationState(this.token.getUser().id).subscribe({
+      next:(state)=>{
+        //this.allowNotifications = state
+      }
+    })
+    this.applicantSer.getProfileDetails(this.token.getUser().id).subscribe({
+      next:(profile)=>{
+        //this.applicantDetails = profile
+      }
+    })
+    this.applicantSer.getWorkExperience(this.token.getUser().id).subscribe({
+      next:(exp)=>{
+        //this.applicantWorkExperience = exp
+      }
+    })
+  }
+
+}
